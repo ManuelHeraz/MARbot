1a272b273963c4bbbbb367945c7579d6c0ac36f7
ManuelHeraz <hernandezdiazjm00@gmail.com>
2026-07-27 13:56:03 -0600

    especioficada la ruta de mongodb para la radio

--- src/database/radioDb.js
+++ src/database/radioDb.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
-// 1. Establecemos la conexión dedicada a la radio usando la URI exclusiva del entorno
-const radioConnection = mongoose.createConnection(process.env.MONGODB_URI_RADIO);
+// 1. Establecemos la conexión y FORZAMOS el nombre de la base de datos
+const radioConnection = mongoose.createConnection(process.env.MONGODB_URI_RADIO, {
+    dbName: 'marina_radio' // <--- Este es el anclaje definitivo
+});
 
 radioConnection.on('connected', () => {
     console.log('📻 Frecuencia de Marina Radio establecida (MongoDB Conectado a marina_radio).');
@@ -19,9 +21,7 @@ const colaSchema = new mongoose.Schema({
     fecha_solicitud: { type: Date, default: Date.now }
 }, { versionKey: false });
 
-// 3. ENRUTAMIENTO EXPLÍCITO: 
-// Asociamos el modelo directamente a 'radioConnection' para que inserte 
-// obligatoriamente en la base de datos de la URI de la radio y en la colección 'cola_reproduccion'.
+// 3. ENRUTAMIENTO EXPLÍCITO
 const ColaReproduccion = radioConnection.model('ColaReproduccion', colaSchema, 'cola_reproduccion');
 
 module.exports = { ColaReproduccion };
